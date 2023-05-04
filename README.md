cf8a7f3abc6a8ac325531e53eb32b4981b1e50eb
leoTomio <leo__tomio@hotmail.com>
2023-05-04 15:50:40 -0300

    Rota de login, arquivos de permissao de usuario

---
+++ README.md
@@ -0,0 +1,6 @@
+Projeto da pizzaria mobile
+
+Passo a passo para executar:
+
+1)Abra o cmd no caminho do projeto e digite expo start --tunnel<br>
+2)Baixe o expo no celular e escanei o qrcode
---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,15 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+
+import Dashboard from '../pages/DashBoard';
+const Stack = createNativeStackNavigator();
+
+function AppRoutes() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Dashboard" component={Dashboard} />
+        </Stack.Navigator>
+    )
+}
+
+export default AppRoutes;
